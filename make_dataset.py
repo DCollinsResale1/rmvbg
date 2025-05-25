7828d0c01cca881bc7136da34bf86f92e451fa1c
DCollinsResale1 <dellon@resale1.com>
2025-05-25 10:08:46 -0400

    Update make_dataset.py

--- make_dataset.py
+++ make_dataset.py
@@ -1,5 +1,8 @@
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
+from skl2onnx import convert_sklearn
+from skl2onnx.common.data_types import FloatTensorType
+
 iris = load_iris()
 X, y = iris.data, iris.target
 X_train, X_test, y_train, y_test = train_test_split(X, y)
